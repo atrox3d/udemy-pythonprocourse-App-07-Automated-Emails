cd3207ec2d7dcb999d8be350dcc644e0ad3a5958
atrox3d <atrox3d@gmail.com>
2022-09-09 10:50:00 +0200

    144. Getting All the News of Today

--- newsfeed.py
+++ newsfeed.py
@@ -1,3 +1,8 @@
+import requests
+from pprint import pprint
+from secret.newsapi_apikey import API_KEY
+
+
 class NewsFeed:
 
     def __init__(self, data):
@@ -8,4 +13,22 @@ class NewsFeed:
 
 
 if __name__ == '__main__':
-    pass
+    base_url = 'https://newsapi.org/v2'
+    endpoint = 'everything'
+    topic = 'cats'
+    from_date = '2022-08-10'
+    to_date = '2022-09-08'
+    sort_by = 'popularity'
+
+    url = (f'{base_url}/'
+           f'{endpoint}?q='
+           f'{topic}&'
+           f'from={to_date}&'
+           # f'to={to_date}&'
+           f'sortBy={sort_by}&'
+           f'apiKey={API_KEY}'
+           )
+
+    print(f'url: {url}')
+    response = requests.get(url)
+    pprint(response.text)
