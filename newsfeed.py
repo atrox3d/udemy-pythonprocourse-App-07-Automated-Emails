1f8b618c95ca5f016982e58e3f273197b46b93f0
atrox3d <atrox3d@gmail.com>
2022-09-09 11:39:27 +0200

    145. Getting News for Certain Dates

--- newsfeed.py
+++ newsfeed.py
@@ -1,5 +1,8 @@
 import requests
 from pprint import pprint
+import datetime
+import dateutil.relativedelta
+
 from secret.newsapi_apikey import API_KEY
 
 
@@ -16,19 +19,26 @@ if __name__ == '__main__':
     base_url = 'https://newsapi.org/v2'
     endpoint = 'everything'
     topic = 'cats'
-    from_date = '2022-08-10'
-    to_date = '2022-09-08'
-    sort_by = 'popularity'
 
-    url = (f'{base_url}/'
-           f'{endpoint}?q='
-           f'{topic}&'
-           f'from={to_date}&'
-           # f'to={to_date}&'
-           f'sortBy={sort_by}&'
-           f'apiKey={API_KEY}'
-           )
+    today = datetime.date.today()                                               # today
+    onemonth = dateutil.relativedelta.relativedelta(months=1)                   # +/- 1 month
+    from_date = (today - onemonth).isoformat()                                  # 1 month ago YY-mm-dd
+    to_date = today.isoformat()                                                 # today YY-mm-dd
+
+    sort_by = 'popularity'
+    language = 'en'
+
+    url = (
+        f'{base_url}/'
+        f'{endpoint}?q='
+        f'{topic}&'
+        f'from={from_date}&'
+        f'to={to_date}&'
+        f'sortBy={sort_by}&'
+        f'language={language}&'
+        f'apiKey={API_KEY}'
+        )
 
     print(f'url: {url}')
-    response = requests.get(url)
-    pprint(response.text)
+    # response = requests.get(url)
+    # pprint(response.text)
