fb9c3e9d2e086d9c2deed730075aceafe2aee1c9
atrox3d <atrox3d@gmail.com>
2022-09-12 10:32:43 +0200

    153. Running the Code at a Certain Time

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 import yagmail
 import pandas
 
@@ -6,30 +9,37 @@ from news import NewsFeed
 from secret.google_app_password import USER, PASSWORD
 # from secret.mail_recipients import RECIPIENTS
 
-#
-# !!!open dropmail.me and update xlsx!!!
-#
-df = pandas.read_excel('data/people.xlsx')                      # !!!open dropmail.me and update xlsx!!!
-for index, row in df.iterrows():                                # loop through rows of xlsx
-    newsfeed = NewsFeed(                                        # for each row create a newsapi request
-                        interest=row['interest'],
-                        from_date=NewsFeed.yesterday(),
-                        to_date=NewsFeed.today(),
-    )
+while True:                                                         # loop forever
+    if (                                                            # runs at 08:00 every day
+            datetime.datetime.now().hour == 8 and
+            datetime.datetime.now().minute == 0
+    ):
+        #
+        # !!!open dropmail.me and update xlsx!!!
+        #
+        df = pandas.read_excel('data/people.xlsx')                  # !!!open dropmail.me and update xlsx!!!
+        for index, row in df.iterrows():                            # loop through rows of xlsx
+            newsfeed = NewsFeed(                                    # for each row create a newsapi request
+                                interest=row['interest'],
+                                from_date=NewsFeed.yesterday(),
+                                to_date=NewsFeed.today(),
+            )
 
-    email = yagmail.SMTP(user=USER, password=PASSWORD)          # connect to gmail sender
-    print(
-        f'{"MAIN":<14.14} | sending mail :'
-        f'MAIN | about "{row["interest"]}" '
-        f'MAIN | to "{row["name"]}"'
-        f'MAIN | at "{row["email"]}"'
-    )
+            email = yagmail.SMTP(user=USER, password=PASSWORD)      # connect to gmail sender
+            print(
+                f'{"MAIN":<14.14} | sending mail :'
+                f'MAIN | about "{row["interest"]}" '
+                f'MAIN | to "{row["name"]}"'
+                f'MAIN | at "{row["email"]}"'
+            )
 
-    mailbody = newsfeed.get_mailbody(use_cache=False)
-    email.send(                                                 # send newsletter
-        to=row['email'],
-        subject=f'Your {row["interest"]} news for today!',
-        contents=f'Hi, {row["name"]}, see what\'s on about {row["interest"]} today.\n'
-                 f'{mailbody}\n'
-                 f'Ardit'
-    )
+            mailbody = newsfeed.get_mailbody(use_cache=False)
+            email.send(                                             # send newsletter
+                to=row['email'],
+                subject=f'Your {row["interest"]} news for today!',
+                contents=f'Hi, {row["name"]},\n'
+                         f'see what\'s on about {row["interest"]} today.\n\n'
+                         f'{mailbody}\n'
+                         f'Ardit'
+            )
+        time.sleep(60)                                              # wait for a minute
