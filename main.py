190731fafa38721ed5f2ffd1f7d4e1f28544bf50
atrox3d <atrox3d@gmail.com>
2022-09-12 10:05:24 +0200

    152. Dynamic Timestamps for Today and Yesterday

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import pandas
 from news import NewsFeed
 
 from secret.google_app_password import USER, PASSWORD
-from secret.mail_recipients import RECIPIENTS
+# from secret.mail_recipients import RECIPIENTS
 
-
-df = pandas.read_excel('data/people.xlsx')
-for index, row in df.iterrows():
-    newsfeed = NewsFeed(
+#
+# !!!open dropmail.me and update xlsx!!!
+#
+df = pandas.read_excel('data/people.xlsx')                      # !!!open dropmail.me and update xlsx!!!
+for index, row in df.iterrows():                                # loop through rows of xlsx
+    newsfeed = NewsFeed(                                        # for each row create a newsapi request
                         interest=row['interest'],
-                        from_date='2022-09-11',
-                        to_date='2022-09-12',
+                        from_date=NewsFeed.yesterday(),
+                        to_date=NewsFeed.today(),
     )
 
-    email = yagmail.SMTP(user=USER, password=PASSWORD)
-    print(f'sending mail about "{row["interest"]}" to "{row["email"]}"')
-    email.send(
-        # to=RECIPIENTS,
+    email = yagmail.SMTP(user=USER, password=PASSWORD)          # connect to gmail sender
+    print(
+        f'{"MAIN":<14.14} | sending mail :'
+        f'MAIN | about "{row["interest"]}" '
+        f'MAIN | to "{row["name"]}"'
+        f'MAIN | at "{row["email"]}"'
+    )
+
+    mailbody = newsfeed.get_mailbody(use_cache=False)
+    email.send(                                                 # send newsletter
         to=row['email'],
         subject=f'Your {row["interest"]} news for today!',
         contents=f'Hi, {row["name"]}, see what\'s on about {row["interest"]} today.\n'
-                 f'{newsfeed.get_mailbody(use_cache=False)}\n'
+                 f'{mailbody}\n'
                  f'Ardit'
     )
