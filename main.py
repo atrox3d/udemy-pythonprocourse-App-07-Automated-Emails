e469ace78010a0f2124a29138b41da2904b9a8e2
atrox3d <atrox3d@gmail.com>
2022-09-12 10:45:38 +0200

    155. Refactoring Our Code

--- main.py
+++ main.py
@@ -9,37 +9,49 @@ from news import NewsFeed
 from secret.google_app_password import USER, PASSWORD
 # from secret.mail_recipients import RECIPIENTS
 
-while True:                                                         # loop forever
-    if (                                                            # runs at 08:00 every day
-            datetime.datetime.now().hour == 8 and
+
+def send_newsletter():
+    newsfeed = NewsFeed(                                            # for each row create a newsapi request
+        interest=row['interest'],
+        from_date=NewsFeed.yesterday(),
+        to_date=NewsFeed.today(),
+    )
+    email = yagmail.SMTP(user=USER, password=PASSWORD)              # connect to gmail sender
+    print(
+        f'{"MAIN":<14.14} | sending mail :'
+        f'MAIN | about "{row["interest"]}" '
+        f'MAIN | to "{row["name"]}"'
+        f'MAIN | at "{row["email"]}"'
+    )
+    mailbody = newsfeed.get_mailbody(use_cache=False)               # get text of mail body
+    email.send(                                                     # send newsletter
+        to=row['email'],
+        subject=f'Your {row["interest"]} news for today!',
+        contents=f'Hi, {row["name"]},\n'
+                 f'see what\'s on about {row["interest"]} today.\n\n'
+                 f'{mailbody}\n'
+                 f'Ardit'
+    )
+
+
+def check_time(hour, minutes):
+    return (
+            datetime.datetime.now().hour == minutes and
             datetime.datetime.now().minute == 0
-    ):
+    )
+
+
+################################################################################
+#
+# main loop
+#
+################################################################################
+while True:                                                         # loop forever
+    if check_time(8, 0):                                            # runs at 08:00 every day
         #
         # !!!open dropmail.me and update xlsx!!!
         #
         df = pandas.read_excel('data/people.xlsx')                  # !!!open dropmail.me and update xlsx!!!
         for index, row in df.iterrows():                            # loop through rows of xlsx
-            newsfeed = NewsFeed(                                    # for each row create a newsapi request
-                                interest=row['interest'],
-                                from_date=NewsFeed.yesterday(),
-                                to_date=NewsFeed.today(),
-            )
-
-            email = yagmail.SMTP(user=USER, password=PASSWORD)      # connect to gmail sender
-            print(
-                f'{"MAIN":<14.14} | sending mail :'
-                f'MAIN | about "{row["interest"]}" '
-                f'MAIN | to "{row["name"]}"'
-                f'MAIN | at "{row["email"]}"'
-            )
-
-            mailbody = newsfeed.get_mailbody(use_cache=False)
-            email.send(                                             # send newsletter
-                to=row['email'],
-                subject=f'Your {row["interest"]} news for today!',
-                contents=f'Hi, {row["name"]},\n'
-                         f'see what\'s on about {row["interest"]} today.\n\n'
-                         f'{mailbody}\n'
-                         f'Ardit'
-            )
+            send_newsletter()
         time.sleep(60)                                              # wait for a minute
